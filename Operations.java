fb3caf1d900723115a02b19954ce97f0ac249b95
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:20:31 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,14 @@
+class Operations
+  {
+    public static void main(String args[])
+
+    {
+      int a=100, b=300;
+      System.out.println("The Addition of "+a+" and "+b+" are" +(a+b));
+       System.out.println("The Substraction of "+a+" and "+b+" are" +(a-b));
+       System.out.println("The Multiplication of "+a+" and "+b+" are "+(a+b));
+       System.out.println("The Division of "+a+" and "+b+" are" +(a/b));
+      System.out.println("The Reminder of "+a+" and "+b+" are" +(a%b));
+      
+    }
+  }
