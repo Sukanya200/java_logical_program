cee3ae14eced43bbb1e3aa4ba93f2ec02e82624d
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:21:24 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test
+  {
+    public static void main(String args[])
+    {
+      float ft=10.23F;
+      double d=2343.43;
+      
+      System.out.println("The Decimal value is:"+ft);
+      System.out.println("The Decimal value is:"+d);
+    }
+  }
