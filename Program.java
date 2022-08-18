12ca98b0854d1f7239e149519251e3bc4adfe4fd
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:21:00 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,10 @@
+class Program
+{
+public static void main(string args[])
+{
+int a=100,b=200;
+System.out.println("The a value is:"+a);
+System.out.println("The b value is:"+b);
+System.out.println("The addition value is:"+(a+b));
+}
+}
