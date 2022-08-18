ff4bdfaf61f31f152ce50318ae2e4f7d1e6782d9
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:19:51 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,10 @@
+class area {
+  public static void main(String args[])
+    {
+       int r=10;
+      double area;
+     System.out.println("The area of the circle is");
+      area=3.147*r*r;
+      System.out.println(area);
+  }
+  }
