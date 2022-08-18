14e83c5327cc751dce96a25ddd3c0f60353f160e
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:18:55 +0530

    Add files via upload

---
+++ emp.java
@@ -0,0 +1,13 @@
+class emp
+  {
+    public static void main (String args[])
+    {
+      int eno=101;
+      String ename="KARUN";
+      float esalary=454.45F;
+      
+      System.out.println("the employee number is"+eno);
+      System.out.println("the employee name is"+ename);
+      System.out.println("the employee salary is"+esalary);
+    }
+  }
