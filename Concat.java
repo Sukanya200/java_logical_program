da25ea1e5408fd84ed6e946eb86b17072f967fb8
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-18 22:19:29 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,11 @@
+class Concat
+{
+public static void main(String args[])
+{
+String st="sukanya";
+String st1= new String("Hello");
+
+System.out.println("The Given Name is:"+st);
+System.out.println("The Given tring is:"+st1);
+}
+}
