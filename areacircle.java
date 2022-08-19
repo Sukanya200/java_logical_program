204cafec9528a25971eb856fa05342228f10db00
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-19 11:01:46 +0530

    Add files via upload

---
+++ areacircle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class areacircle
+  {
+    public static void main (String args[])
+    {
+      int r;
+      double pi=3.14,area;
+      Scanner sc =new Scanner(System.in);
+      System.out.print("Enter radius of circle:");
+      r=sc.nextInt();
+      area=pi*r*r;
+      System.out.println("Area of circle:"+area);
+      
+    }
+  }
