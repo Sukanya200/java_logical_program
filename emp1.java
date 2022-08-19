fb5d2c57695d8ffc174df10f315d03c6a9b5766d
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-19 11:02:32 +0530

    Add files via upload

---
+++ emp1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class First
+  {
+    public static void main(String args[])
+    {
+      int eno;
+      String ename;
+      float esal;
+      Scanner sc=new Scanner(System.in);
+
+      System.out.println("Enter Employee Number:");
+      eno=sc.nextInt();
+      System.out.println("Enter Employee Name:");
+      ename=sc.next();
+      System.out.println("Enter Employee Salary:");
+      esal=sc.nextFloat();
+
+       System.out.println("the employee number is"+eno);
+      System.out.println("the employee name is"+ename);
+      System.out.println("the employee salary is"+esalary);
+    }
+  }
