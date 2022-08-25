57e9f4045a2de3f81764062aa783e533f6ec9e2e
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-08-25 12:49:29 +0530

    Add files via upload

---
+++ Large.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ class Large
+  {
+    public static void main(String args[])
+    {
+      int a,b,c;
+      Scanner sc= new Scanner(System.in);
+      if(a>=b)
+      {
+        System.out.println("a is big");
+        else
+      {
+        System.out.println("b is big");
+        
+      }
+      if(b)
+      }
+    }
+  }
