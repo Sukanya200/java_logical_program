7e4fd313705a3cccf8b1bb1314779906d129f822
Sukanya200 <104908097+Sukanya200@users.noreply.github.com>
2022-10-10 18:48:37 +0530

    Add files via upload

---
+++ students.java
@@ -0,0 +1,28 @@
+class students
+{
+    int sno;
+    String sname;
+    double savg;
+    students()
+    {
+      sno=10;
+      sname="sukanya";
+      savg=60.0;
+      
+    }
+    void display()
+    {
+      System.out.println("Student number is:"+sno);
+      System.out.println("Student name is:"+sname);
+      System.out.println("Student average is:"+savg);
+    }class students
+  
+  }
+class students
+  {
+    public static void main(String[] args)
+    {
+      studens1 s=new studens1();
+      s.display();
+    }
+  }
